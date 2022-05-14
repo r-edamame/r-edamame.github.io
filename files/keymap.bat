d773368d1f9891704186bb5f8feb9b611ed1f804
edamame <edamame15.r@gmail.com>
2022-05-14 22:06:29 +0900

    keymap

---
+++ files/keymap.bat
@@ -0,0 +1,6 @@
+Windows Registry Editor Version 5.00
+
+ 
+
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout]
+"Scancode Map"=hex:00,00,00,00,00,00,00,00,04,00,00,00,39,00,2a,00,2a,00,39,00,1d,00,3a,00,00,00,00,00
